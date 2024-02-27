12de4e2460ce79d192ed6fd23e8fbeb55fb61b38
xujiayu <xujiayu@motern.com>
2024-02-27 11:04:54 +0800

    new branch

---
+++ src/main/java/com/whut/pan/model/ResponseMsgAdd.java
@@ -0,0 +1,17 @@
+package com.whut.pan.model;
+
+/**
+ * Created by zc on 2018/11/2.
+ */
+public class ResponseMsgAdd extends ResponseMsg {
+
+    private Object obj;
+
+    public Object getObj() {
+        return obj;
+    }
+
+    public void setObj(Object obj) {
+        this.obj = obj;
+    }
+}
